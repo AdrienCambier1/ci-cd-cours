c509192a505c68179f3bc0551331050387dce64a
Adrien Cambier <adriencambier04@gmail.com>
2026-05-07 10:00:58 +0200

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
+  base: '/ci-cd-cours/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
