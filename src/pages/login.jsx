db12da287f853cd78dc57139e63a266f2a27d859
Adrien Cambier <adriencambier04@gmail.com>
2026-06-18 10:22:22 +0200

    feat: enhance login page layout with flexbox for better centering

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -2,7 +2,7 @@ import LoginForm from "../components/login-form";
 
 function Login({ onLogin }) {
   return (
-    <main className="min-h-full w-full p-6">
+    <main className="min-h-full w-full p-6 flex items-center justify-center">
       <LoginForm onLogin={onLogin} />
     </main>
   );
